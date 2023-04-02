f70210b10c986d4268be23f6790905cc26340905
xinlingduyu <52751232+xinlingduyu@users.noreply.github.com>
2023-04-02 11:41:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,6 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' >>feeds.conf.default
-echo 'src-git luci https://github.com/Lienol/openwrt-luci.git;22.03' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03' >>feeds.conf.default
-echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >>feeds.conf.default
-echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
 echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
 echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
